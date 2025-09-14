6022639c097bcd9c71618e9e1e597b6249301474
Jay Gajera <jay.g@aubergine.co>
2025-09-14 20:43:24 +0530

    TDD cycle5 - Refactor: created methods for better redability

--- src/app/Utils.ts
+++ src/app/Utils.ts
@@ -1,10 +1,24 @@
+import { DELIMITERS } from "./constant";
+
 export class StringCalculator {
   add(numbers: string): number {
-    if (numbers === '') {
+    if (this.isEmpty(numbers)) {
       return 0;
     }
 
-    const nums = numbers.split(/,|\n/);
-    return nums.reduce((sum, num) => sum + parseInt(num), 0);
+    const numberArray = this.parseNumbers(numbers);
+    return this.sumNumbers(numberArray);
+  }
+
+  private isEmpty(input: string): boolean {
+    return input === '';
+  }
+
+  private parseNumbers(numbers: string): number[] {
+    return numbers.split(DELIMITERS).map(num => parseInt(num));
+  }
+
+  private sumNumbers(numbers: number[]): number {
+    return numbers.reduce((sum, num) => sum + num, 0);
   }
 }
